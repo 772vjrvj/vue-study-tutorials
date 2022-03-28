edc1dab4834adc550081b831d0fe7f7c43320253
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2022-03-28 21:09:37 +0900

    ddd

---
+++ vue2-study-testcomponent/src/app/app.vue
@@ -0,0 +1,88 @@
+<!-- https://idlecomputer.tistory.com/181 -->
+<!-- https://velog.io/@mintaeho75/Vue.js-%EA%B8%B0%EC%B4%88-%EC%8A%AC%EB%A1%AF%EA%B3%BC-%EB%AF%B9%EC%8A%A4%EC%9D%B8 -->
+<!-- https://kr.vuejs.org/v2/guide/components-slots.html -->
+
+
+<template>
+  <div>
+
+    <slotButtonVuejsTest>
+      Save
+    </slotButtonVuejsTest>
+
+    <slotContentsVuejsTest>
+      <h1 slot="header">Here might be a page title</h1>
+
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+
+      <p slot="footer">Here's some contact info</p>
+    </slotContentsVuejsTest>
+    
+    <div>-----1</div>
+
+    <slotScopeVuejsTest :todos="todos">
+      <li slot="litodo" slot-scope="slotProps" v-if="slotProps.todo.isCompleted">
+        <span>V--</span>{{ slotProps.todo.text }}
+      </li>
+    </slotScopeVuejsTest>
+
+    <div>-----2</div>
+
+    <slotScopeVuejsTest :todos="todos">
+      <template v-slot:litodo="{todo}">
+        <li v-if="todo.isCompleted">
+          <span>V--</span>{{ todo.text }}
+        </li>
+      </template>
+    </slotScopeVuejsTest>
+
+    <div>-----3</div>
+
+    <slotScopeBasicArrayTest :items="items">
+      <template slot="menu-item" slot-scope="{item}">
+        <div>
+          <em v-text="item"></em>
+        </div>
+      </template>
+    </slotScopeBasicArrayTest>
+
+    <slotScopeBasicArrayTest :items="items">
+      <template v-slot:menu-item="{item}">
+        <div>
+          <em v-text="item"></em>
+        </div>
+      </template>
+    </slotScopeBasicArrayTest>
+
+
+  </div>
+</template>
+
+<script>
+import slotButtonVuejsTest from '@/components/slot/slotButtonVuejsTest';
+import slotContentsVuejsTest from '@/components/slot/slotContentsVuejsTest';
+import slotScopeVuejsTest from '@/components/slot/slotScopeVuejsTest';
+import slotScopeBasicArrayTest from '@/components/slot/slotScopeBasicArrayTest';
+
+export default {
+  data() {
+    return {
+        todos: [
+            { id: 1, text: 'C++', isCompleted: true },
+            { id: 2, text: 'JavaScript', isCompleted: false },
+            { id: 3, text: 'Java', isCompleted: true },
+            { id: 4, text: 'Perl', isCompleted: false },
+        ],
+        items: ['one', 'two', 'three']
+    }
+  },
+  components: {
+    slotButtonVuejsTest,
+    slotContentsVuejsTest,
+    slotScopeVuejsTest,
+    slotScopeBasicArrayTest
+  }
+}
+</script>
+ 
---
+++ vue2-study-testcomponent/src/components/slot/slotButtonVuejsTest.vue
@@ -0,0 +1,11 @@
+<!-- https://kr.vuejs.org/v2/guide/components-slots.html?#%EB%B2%94%EC%9C%84%EA%B0%80-%EC%9E%88%EB%8A%94-%EC%8A%AC%EB%A1%AF(Scoped-Slots)-->
+<template>
+  <button type="submit">
+    <slot>Submit</slot>
+  </button>
+</template>
+
+<script>
+
+</script>
+ 
